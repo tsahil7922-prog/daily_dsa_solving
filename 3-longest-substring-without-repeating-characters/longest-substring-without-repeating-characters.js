de8b658738cabe4e8514dc2951f39d544e86e6e5
tsahil7922-prog <tsahil7922@gmail.com>
2026-09-07 14:35:41 +0530

    Time: 20 ms (71.37%) | Memory: 57.3 MB (90.47%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -3,19 +3,19 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function (s) {
-    let max=0
-    let left=0
-    for(let i=0;i<s.length;i++){
-        for(let j=left;j<i;j++){
-            if(s[j]===s[i]){
-                left = j+1
-            break;
+    let max = 0
+    let left = 0
+    for (let i = 0; i < s.length; i++) {
+        for (let j = left; j < i; j++) {
+            if (s[j] === s[i]) {
+                left = j + 1
+                break;
             }
-            
+
         }
-        let length = i-left +1
-        if(length>max){
-            max=length
+        let length = i - left + 1
+        if (length > max) {
+            max = length
         }
     }
     return max
