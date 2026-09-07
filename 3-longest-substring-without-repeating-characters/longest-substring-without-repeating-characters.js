ba3161f178448577e3c690eaf2a47b594f2e1157
tsahil7922-prog <tsahil7922@gmail.com>
2026-09-07 14:35:34 +0530

    Time: 20 ms (71.37%) | Memory: 58.3 MB (82.22%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+    let max=0
+    let left=0
+    for(let i=0;i<s.length;i++){
+        for(let j=left;j<i;j++){
+            if(s[j]===s[i]){
+                left = j+1
+            break;
+            }
+            
+        }
+        let length = i-left +1
+        if(length>max){
+            max=length
+        }
+    }
+    return max
+
+};
